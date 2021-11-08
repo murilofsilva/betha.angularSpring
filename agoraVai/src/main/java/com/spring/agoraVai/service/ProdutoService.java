ddc3d33140636cbdd94a40ef89b7af53a04274de
Murilo <67132341+murilofsilva@users.noreply.github.com>
2021-11-08 09:18:22 -0400

    Add files via upload

---
+++ agoraVai/src/main/java/com/spring/agoraVai/service/ProdutoService.java
@@ -0,0 +1,42 @@
+package com.spring.agoraVai.service;
+
+import com.spring.agoraVai.model.Produtos;
+import com.spring.agoraVai.repo.ProdutoRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.nio.file.attribute.UserPrincipalNotFoundException;
+import java.util.List;
+
+@Service
+@Transactional
+public class ProdutoService {
+    private final ProdutoRepo produtoRepo;
+
+    @Autowired
+    public ProdutoService(ProdutoRepo produtoRepo){
+        this.produtoRepo = produtoRepo;
+    }
+
+    public Produtos addProduto(Produtos produto){
+        return produtoRepo.save(produto);
+    }
+
+    public List<Produtos> getAllProdutos(){
+        return this.produtoRepo.findAll();
+    }
+
+    public void deleteProduto(Long id){
+        produtoRepo.deleteProdutoById(id);
+    }
+
+    public Produtos updateProduto(Produtos produtos) {
+        return produtoRepo.save(produtos);
+    }
+
+    public Produtos findProdutoById(Long id) throws UserPrincipalNotFoundException {
+        return produtoRepo.findProdutoById(id).orElseThrow(() ->
+        new UserPrincipalNotFoundException("Não encontrado"));
+    }
+}
